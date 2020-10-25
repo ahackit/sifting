479b3496877c5d176f394541e7c4435e3a5cf7d9
Austin Hackett <ahackit@gmail.com>
2020-10-25 16:03:04 -0500

    Adds Populate Data management command for initial data.

--- backend/sift/management/commands/populate_data.py
+++ backend/sift/management/commands/populate_data.py
@@ -1,4 +1,3 @@
-import lorem
 import random
 import datetime
 
@@ -8,64 +7,65 @@ from ...models import Difficulty, Cuisine, UOM, Grocery, Cookware
 
 
 def populate_difficulty():
-    Difficulty.objects.create(type_txt='Easy')
-    Difficulty.objects.create(type_txt='Medium')
-    Difficulty.objects.create(type_txt='Hard')
+    Difficulty.objects.get_or_create(type_txt='Easy')
+    Difficulty.objects.get_or_create(type_txt='Medium')
+    Difficulty.objects.get_or_create(type_txt='Hard')
 
 
 def populate_cuisine():
-    Cuisine.objects.create(type_txt='Mexican')
-    Cuisine.objects.create(type_txt='Italian')
-    Cuisine.objects.create(type_txt='American')
-    Cuisine.objects.create(type_txt='Chinese')
-    Cuisine.objects.create(type_txt='Thai')
-    Cuisine.objects.create(type_txt='Greek')
-    Cuisine.objects.create(type_txt='Indian')
+    Cuisine.objects.get_or_create(type_txt='Mexican')
+    Cuisine.objects.get_or_create(type_txt='Italian')
+    Cuisine.objects.get_or_create(type_txt='American')
+    Cuisine.objects.get_or_create(type_txt='Chinese')
+    Cuisine.objects.get_or_create(type_txt='Thai')
+    Cuisine.objects.get_or_create(type_txt='Greek')
+    Cuisine.objects.get_or_create(type_txt='Indian')
 
 
 def populate_uom():
-    UOM.objects.create(measurement='teaspoon')
-    UOM.objects.create(measurement='tablespoon')
-    UOM.objects.create(measurement='fluid ounce')
-    UOM.objects.create(measurement='gill')
-    UOM.objects.create(measurement='cup')
-    UOM.objects.create(measurement='pint')
-    UOM.objects.create(measurement='quart')
-    UOM.objects.create(measurement='gallon')
-    UOM.objects.create(measurement='ml')
-    UOM.objects.create(measurement='l')
-    UOM.objects.create(measurement='dl')
-    UOM.objects.create(measurement='pound')
-    UOM.objects.create(measurement='ounce')
-    UOM.objects.create(measurement='mg')
-    UOM.objects.create(measurement='g')
-    UOM.objects.create(measurement='kg')
-    UOM.objects.create(measurement='mm')
-    UOM.objects.create(measurement='cm')
-    UOM.objects.create(measurement='m')
-    UOM.objects.create(measurement='inch')
+    UOM.objects.get_or_create(measurement='teaspoon')
+    UOM.objects.get_or_create(measurement='tablespoon')
+    UOM.objects.get_or_create(measurement='fluid ounce')
+    UOM.objects.get_or_create(measurement='gill')
+    UOM.objects.get_or_create(measurement='cup')
+    UOM.objects.get_or_create(measurement='pint')
+    UOM.objects.get_or_create(measurement='quart')
+    UOM.objects.get_or_create(measurement='gallon')
+    UOM.objects.get_or_create(measurement='ml')
+    UOM.objects.get_or_create(measurement='l')
+    UOM.objects.get_or_create(measurement='dl')
+    UOM.objects.get_or_create(measurement='pound')
+    UOM.objects.get_or_create(measurement='ounce')
+    UOM.objects.get_or_create(measurement='mg')
+    UOM.objects.get_or_create(measurement='g')
+    UOM.objects.get_or_create(measurement='kg')
+    UOM.objects.get_or_create(measurement='mm')
+    UOM.objects.get_or_create(measurement='cm')
+    UOM.objects.get_or_create(measurement='m')
+    UOM.objects.get_or_create(measurement='inch')
+    UOM.objects.get_or_create(measurement='can')
 
 
 def populate_grocery():
-    Grocery.objects.create(type_txt='beverages')
-    Grocery.objects.create(type_txt='bakery')
-    Grocery.objects.create(type_txt='canned good')
-    Grocery.objects.create(type_txt='dairy')
-    Grocery.objects.create(type_txt='dry/baking good')
-    Grocery.objects.create(type_txt='frozen foods')
-    Grocery.objects.create(type_txt='produce')
+    Grocery.objects.get_or_create(type_txt='beverages')
+    Grocery.objects.get_or_create(type_txt='bakery')
+    Grocery.objects.get_or_create(type_txt='canned good')
+    Grocery.objects.get_or_create(type_txt='dairy')
+    Grocery.objects.get_or_create(type_txt='dry/baking good')
+    Grocery.objects.get_or_create(type_txt='frozen foods')
+    Grocery.objects.get_or_create(type_txt='produce')
 
 
 def populate_cookware():
 
-    Cookware.objects.create(name="stock pot")
-    Cookware.objects.create(name="frying pan")
-    Cookware.objects.create(name="cast iron skillet")
-    Cookware.objects.create(name="saute pan")
-    Cookware.objects.create(name="Sauce pan")
-    Cookware.objects.create(name="slower cooker")
-    Cookware.objects.create(name="roasting pan")
-    Cookware.objects.create(name="dutch oven")
+    Cookware.objects.get_or_create(name="stock pot")
+    Cookware.objects.get_or_create(name="frying pan")
+    Cookware.objects.get_or_create(name="cast iron skillet")
+    Cookware.objects.get_or_create(name="saute pan")
+    Cookware.objects.get_or_create(name="Sauce pan")
+    Cookware.objects.get_or_create(name="slower cooker")
+    Cookware.objects.get_or_create(name="roasting pan")
+    Cookware.objects.get_or_create(name="dutch oven")
 
 
 class Command(BaseCommand):
