879745eda0106fc6a43f25cbcf64c803527d8f86
Austin Hackett <ahackit@gmail.com>
2020-11-03 08:07:15 -0600

    Finish Calculator.

--- frontend/pages/uomcalc/index.vue
+++ frontend/pages/uomcalc/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-row">
     <div class="w-1/2 mx-auto flex flex-row justify-center mt-8">
       <div>
         <p>Convert</p>
@@ -12,7 +12,7 @@
         />
         <select class="border" name="uom-type" id="uom-type" v-model="startUOM">
           <option
-            v-for="option in options"
+            v-for="option in volumeUOMs"
             :value="option.measurement"
             :key="option.id"
             >{{ option.measurement }}</option
@@ -21,7 +21,7 @@
         <p>To</p>
         <select class="border" name="uom-type" id="uom-type" v-model="endUOM">
           <option
-            v-for="option in options"
+            v-for="option in volumeUOMs"
             :value="option.measurement"
             :key="option.id"
             >{{ option.measurement }}</option
@@ -31,6 +31,49 @@
       </div>
       <div>
         <p>Results: {{ conversionResults }}</p>
+      </div>
+    </div>
+    <div class="w-1/2 mx-auto flex flex-row justify-center mt-8">
+      <div>
+        <p>Convert</p>
+        <input
+          class="border"
+          type="number"
+          name="amount-uom"
+          id="amount-uom"
+          v-model="amountWeightUOM"
+        />
+        <select
+          class="border"
+          name="uom-type"
+          id="uom-type"
+          v-model="starWeightUOM"
+        >
+          <option
+            v-for="option in weightUOMs"
+            :value="option.measurement"
+            :key="option.id"
+            >{{ option.measurement }}</option
+          >
+        </select>
+        <p>To</p>
+        <select
+          class="border"
+          name="uom-type"
+          id="uom-type"
+          v-model="endWeightUOM"
+        >
+          <option
+            v-for="option in weightUOMs"
+            :value="option.measurement"
+            :key="option.id"
+            >{{ option.measurement }}</option
+          >
+        </select>
+        <button @click="getWeightConversion">Generate</button>
+      </div>
+      <div>
+        <p>Results: {{ weightConversionResults }}</p>
       </div>
     </div>
   </div>
@@ -44,7 +87,23 @@ export default {
       startUOM: '',
       endUOM: '',
       amountUOM: 0,
-      conversionResults: ''
+      starWeighttUOM: '',
+      endWeightUOM: '',
+      amountWeightUOM: 0,
+      conversionResults: '',
+      weightConversionResults: ''
+    }
+  },
+  computed: {
+    volumeUOMs() {
+      return this.options.filter(
+        (option) => option.conversion_type === 'volume'
+      )
+    },
+    weightUOMs() {
+      return this.options.filter(
+        (option) => option.conversion_type === 'weight'
+      )
     }
   },
   async mounted() {
@@ -57,6 +116,12 @@ export default {
         `/uoms/convert/?start_uom_number=${this.amountUOM}&start_uom=${this.startUOM}&end_uom=${this.endUOM}`
       )
       this.conversionResults = response.data
+    },
+    async getWeightConversion() {
+      const response = await this.$axios.get(
+        `/uoms/convert/?start_uom_number=${this.amountWeightUOM}&start_uom=${this.starWeightUOM}&end_uom=${this.endWeightUOM}`
+      )
+      this.weightConversionResults = response.data
     }
   }
 }
