58975eb78623ff8b83bb1f404584a50d1600673b
Austin Hackett <ahackit@gmail.com>
2020-12-13 15:42:59 -0600

    Update urls.

--- backend/sifting/urls.py
+++ backend/sifting/urls.py
@@ -23,7 +23,7 @@ router.register(r'recipes', views.RecipeViewSet)
 router.register(r'uoms', views.UOMViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
